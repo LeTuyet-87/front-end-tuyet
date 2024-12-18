71261a581ccdd7fac63eae6ec94c53e4baca3dba
Lê Tuyết <lethituyet872003@gmail.com>
2024-12-18 15:43:50 +0700

    Delete Back end __ staff-management-be-main/backend directory

--- Back end __ staff-management-be-main/backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('user/', include('api.login.urls')),
-    path('employee/', include('api.employee.urls')),
-    path('timesheet/', include('api.timesheet.urls')),
-    path('salary/', include('api.salary.urls')),
-]
